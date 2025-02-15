a3775ed7ede1be18914cefd8e19e4686660704f8
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2025-02-15 08:52:26 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsletterBox'
-import ShopCategories from '../components/ShopCategories'
-import ShopVideo from '../components/ShopVideo'
-import Testimonials from '../components/Testimonials'
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Hero from '../components/Hero';
+import LatestCollection from '../components/LatestCollection';
+import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
+import NewsletterBox from '../components/NewsletterBox';
+import ShopCategories from '../components/ShopCategories';
+import ShopVideo from '../components/ShopVideo';
+import Testimonials from '../components/Testimonials';
+import { ShopContext } from '../context/ShopContext';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { token } = useContext(ShopContext);
+
+  useEffect(() => {
+    if (!token) {
+      // Auto-redirect after 2 minutes if user is not logged in
+      const timer = setTimeout(() => {
+        navigate('/login');
+      }, 120000); // 120000ms = 2 minutes
+
+      return () => clearTimeout(timer); // Cleanup function to clear timeout when component unmounts
+    }
+  }, [token, navigate]);
+
   return (
     <div>
       <Hero />
-      <LatestCollection/>
+      <LatestCollection />
       <BestSeller />
       <ShopCategories />
       <ShopVideo />
       <Testimonials />
-      <OurPolicy/>
+      <OurPolicy />
       {/* <NewsletterBox/> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
